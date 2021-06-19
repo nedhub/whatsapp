f95860bf315d9c11e635ea60ac477a1d79f55b8f
Ned Ememanka <nedememanka@icloud.com>
2021-06-19 19:07:07 -0400

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icon.dart';
+import 'package:whatsapp/pages/statuspage.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      bottomNavigationBar: NavBar(),
+      body: Body(),
+    );
+  }
+}
+
+class NavBar extends StatefulWidget {
+  NavBar({Key? key}) : super(key: key);
+
+  @override
+  _NavBarState createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  int pageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    List iconItems = [
+      Icons.circle_outlined,
+      Icons.phone_sharp,
+      Icons.camera_alt,
+      Icons.comment_sharp,
+      Icons.settings,
+    ];
+    List textItems = ["Status", "Calls", "Camera", "Chats", "Settings"];
+
+    return Container(
+        height: 90,
+        width: double.infinity,
+        decoration: BoxDecoration(color: Colors.grey[900]),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20, top: 5),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: List.generate(textItems.length, (index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      pageIndex = index;
+                    });
+                  },
+                  child: Column(children: [
+                    Icon(iconItems[index],
+                        color: pageIndex == index
+                            ? Colors.white
+                            : Colors.white.withOpacity(0.5)),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(textItems[index],
+                        style: TextStyle(
+                            fontSize: 10, color: Colors.white.withOpacity(0.5)))
+                  ]),
+                );
+              })),
+        ));
+  }
+}
+
+class Body extends StatefulWidget {
+  Body({Key? key}) : super(key: key);
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int pageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return IndexedStack(index: pageIndex, children: [
+      StatusPage(),
+      StatusPage(),
+      StatusPage(),
+      StatusPage(),
+      StatusPage(),
+    ]);
+  }
+}
---
+++ lib/pages/statuspage.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/json/chatdata.dart';
+
+class StatusPage extends StatefulWidget {
+  StatusPage({Key? key}) : super(key: key);
+
+  @override
+  _StatusPageState createState() => _StatusPageState();
+}
+
+class _StatusPageState extends State<StatusPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Row(
+            children: [
+              Text(
+                "Privacy",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.lightBlue,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        body: ListView(children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15, top: 15),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+              Text("Status",
+                  style: TextStyle(
+                      fontSize: 23,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold)),
+              SizedBox(height: 15),
+              Container(
+                height: 38,
+                decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  cursorColor: Colors.grey[300],
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.white12.withOpacity(0.4),
+                      ),
+                      border: InputBorder.none,
+                      hintText: "Search",
+                      hintStyle: TextStyle(
+                        color: Colors.white12.withOpacity(0.4),
+                        fontSize: 17,
+                      )),
+                ),
+              )
+            ]),
+          ),
+          SizedBox(height: 50),
+          Container(
+            height: 80,
+            width: double.infinity,
+            decoration: BoxDecoration(color: Colors.grey[900]),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        child: Stack(
+                          children: [
+                            Container(
+                              width: 70,
+                              height: 70,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                      image: NetworkImage(profile[0]['img']),
+                                      fit: BoxFit.cover)),
+                            ),
+                            Positioned(
+                              right: 5,
+                              bottom: 0,
+                              child: Container(
+                                  width: 20,
+                                  height: 20,
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.green),
+                                  child: Center(
+                                    child: Icon(Icons.add,
+                                        color: Colors.white, size: 18),
+                                  )),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 5),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "My Status",
+                              style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white),
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              "Add to my status",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w100,
+                                  color: Colors.white.withOpacity(0.3)),
+                            ),
+                          ])
+                    ],
+                  ),
+                  Row(children: [
+                    Container(
+                      width: 38,
+                      height: 38,
+                      decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.2),
+                          shape: BoxShape.circle),
+                      child: Center(
+                        child:
+                            Icon(Icons.camera, color: Colors.white, size: 20),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      width: 38,
+                      height: 38,
+                      decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.2),
+                          shape: BoxShape.circle),
+                      child: Center(
+                        child: Icon(Icons.edit, color: Colors.white, size: 20),
+                      ),
+                    )
+                  ])
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+        ]));
+  }
+}
